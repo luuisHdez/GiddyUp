984a19eed173ed110a26c6a49ea1ab6c6877a2d7
Luis hernandez <luiszs.lr@gmail.com>
2024-08-28 13:46:01 -0600

    prueba

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,18 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "dev": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "giddyupe48810e4": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
--- src/lambda-api.js
+++ src/lambda-api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const lambdaapi = axios.create({
-    baseURL: 'https://x103wivm31.execute-api.us-east-1.amazonaws.com/dev'
+    baseURL: 'https://tyksjoc574.execute-api.us-east-1.amazonaws.com/dev'
 })
 
 export const getLambda = () => {
